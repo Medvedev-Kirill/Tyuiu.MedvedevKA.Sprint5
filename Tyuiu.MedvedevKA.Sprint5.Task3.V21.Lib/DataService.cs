fccd003f5088b3afa4d837407ae08ba07559dbb1
Tyuiu.MedvedevKA@outlook.com <medvedevkirill2016@mail.ru>
2023-11-17 20:50:49 +0500

    Task3

---
+++ Tyuiu.MedvedevKA.Sprint5.Task3.V21.Lib/DataService.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.IO;
+
+using tyuiu.cources.programming.interfaces.Sprint5;
+
+namespace Tyuiu.MedvedevKA.Sprint5.Task3.V21.Lib
+{
+    public class DataService : ISprint5Task3V21
+    {
+        public string SaveToFileTextData(int x)
+        {
+            string path = $@"{Directory.GetCurrentDirectory()}\OutPutFileTask3.bin";
+
+            double res = ((double)Math.Pow(x, 2) + 1) / (double)Math.Sqrt(4 * Math.Pow(x, 2) - 3);
+
+            res = Math.Round(res, 3);
+
+            using (BinaryWriter writer = new BinaryWriter(File.Open(path, FileMode.OpenOrCreate), Encoding.UTF8))
+            {
+                writer.Write(BitConverter.GetBytes(res));
+            }
+
+            return path;
+        }
+    }
+}
---
+++ Tyuiu.MedvedevKA.Sprint5.Task3.V21.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.MedvedevKA.Sprint5.Task3.V21.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.MedvedevKA.Sprint5.Task3.V21.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("41628849-1513-4c06-b6f1-49f0f6585181")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
