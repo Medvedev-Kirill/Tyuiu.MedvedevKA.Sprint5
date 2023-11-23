5da3e05f06e20825c93cc57133a6cf59f400e15b
Tyuiu.MedvedevKA@outlook.com <medvedevkirill2016@mail.ru>
2023-11-23 18:47:45 +0500

    Task7

---
+++ Tyuiu.MedvedevKA.Sprint5.Task7.V23.Lib/DataService.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.IO;
+
+using tyuiu.cources.programming.interfaces.Sprint5;
+
+namespace Tyuiu.MedvedevKA.Sprint5.Task7.V23.Lib
+{
+    public class DataService : ISprint5Task7V23
+    {
+        public string LoadDataAndSave(string path)
+        {
+            string pathSaveFile = $@"{Directory.GetCurrentDirectory()}\OutPutFileTask7.txt";
+
+            FileInfo fileInfo = new FileInfo(pathSaveFile);
+            bool fileExixsts = fileInfo.Exists;
+
+            if (fileExixsts)
+            {
+                File.Delete(pathSaveFile);
+            }
+
+            string strLine = "";
+            using (StreamReader reader = new StreamReader(path))
+            {
+                string line;
+                while ((line = reader.ReadLine()) !=null)
+                {
+                    for (int i = 0; i < line.Length; i++)
+                    {
+                        if (!(Convert.ToChar(line[i]) >= 'а' && Convert.ToChar(line[i]) <= 'я' || Convert.ToChar(line[i]) >= 'А' && Convert.ToChar(line[i]) <= 'Я'))
+                        {
+                            strLine = strLine + line[i];
+                        }
+                    }
+                    File.AppendAllText(pathSaveFile, strLine + Environment.NewLine);
+                    strLine = "";
+                }
+            }
+            return pathSaveFile;
+        }
+    }
+}
---
+++ Tyuiu.MedvedevKA.Sprint5.Task7.V23/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.IO;
+
+using Tyuiu.MedvedevKA.Sprint5.Task7.V23.Lib;
+
+namespace Tyuiu.MedvedevKA.Sprint5.Task7.V23
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #5 | Выполнил: Медведев К. А. | ACОиУБ-23-3";
+            //Длина строки 75 символов
+            Console.WriteLine("**********************************************************************************************************************");
+            Console.WriteLine("* Спринт #5                                                                                                          *");
+            Console.WriteLine("* Тема: Добавление к решению итоговых проектов по спринту                                                            *");
+            Console.WriteLine("* Задание #7                                                                                                         *");
+            Console.WriteLine("* Вариант #23                                                                                                        *");
+            Console.WriteLine("* Выполнил: Медведев Кирилл Андриянович | ACOиУБ-23-3                                                                *");
+            Console.WriteLine("**********************************************************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                                                           *");
+            Console.WriteLine("* Дан файл С:/DataSprint5/InPutDataFileTask7V23.txt в котором есть набор символьных данных.                          *");
+            Console.WriteLine("* Удалить все русские слова из файла.Полученный результат сохранить в файл OutPutDataFileTask7V23.txt.               *");
+            Console.WriteLine("**********************************************************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                                                                   *");
+            Console.WriteLine("**********************************************************************************************************************");
+
+            string path = @"C:\DataSprint5\InPutDataFileTask7V23.txt";
+            string pathSaveFile = $@"{Directory.GetCurrentDirectory()}\OutPutFileTask7.txt";
+
+            Console.WriteLine("Данные находятся в файле: " + path);
+            Console.WriteLine("**********************************************************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                                                                         *");
+            Console.WriteLine("**********************************************************************************************************************");
+            Console.WriteLine("Находится в файле: ");
+            pathSaveFile = ds.LoadDataAndSave(path);
+            Console.WriteLine(pathSaveFile);
+            Console.ReadKey();
+        }
+    }
+}
